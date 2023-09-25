7aea9d8866681f7c2613789e376584b7d001c069
Lujan Benjamin <lujanbenjamin1@gmail.com>
2023-09-25 15:27:52 -0300

    a ver si sube

--- src/dao/models/cart.model.js
+++ src/dao/models/cart.model.js
@@ -4,8 +4,17 @@ const cartCollection = 'cart'
 
 const cartSchema = new mongoose.Schema({
     id: String,
-    products:[String] 
-})
+    products:{
+        type: [{
+            _id:false,
+            //aca la refe para que llame a los productos dentro del carrito
+            product: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'product'
+            }
+        }]
+    }}
+)
 
 const cartModel = mongoose.model(cartCollection, cartSchema)
 
